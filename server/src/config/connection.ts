ba24a5e40c0282a9be5e7707f5a0d819a54ad982
Adebanjo Fajemisin <marcusfajemisin@gmail.com>
2025-03-05 19:09:01 -0500

    fixed graphql error

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Adebanjo:OmUiyVkwYG8zP5Fk@cluster05-13.p3mce.mongodb.net/?retryWrites=true&w=majority&appName=Cluster05-13');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Adebanjo:AtP1NxzKBqW0vHuJ@cluster05-13.p3mce.mongodb.net/?retryWrites=true&w=majority&appName=Cluster05-13');
 
 export default mongoose.connection;

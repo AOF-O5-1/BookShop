2e3c7ea4ac3f7cd66b15da088328c010d53e789a
Adebanjo Fajemisin <marcusfajemisin@gmail.com>
2025-03-06 11:47:12 -0500

    render

--- server/src/server.ts
+++ server/src/server.ts
@@ -29,7 +29,7 @@ app.use((req, _res, next) => {
 // Enable CORS for requests from http://localhost:3000
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3001',
     credentials: true,
   })
 );
